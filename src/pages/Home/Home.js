1dc6c6145e601bb9062c922f520d9252864c97dc
kaido <kaido@qi-cloud.com>
2017-11-28 10:37:29 +0800

    add Home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Zep on 2017/11/28.
+ * 
+ */
+
+export default {
+  name: 'home-page',
+  props: {},
+  watch: {},
+  methods: {},
+  computed: {},
+  components: {},
+  data() {},
+  mounted() {},
+  created() {}
+}
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,13 @@
+<!-- Created by Zep on 2017/11/28. -->
+<template>
+  <div class="home-container">Home</div>
+</template>
+<script>
+import Home from './Home.js'
+export default Home
+
+</script>
+<style lang="scss" scoped>
+@import './Home.scss'
+
+</style>
