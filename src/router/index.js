2d8259623767786943aab4511405fc1debedaa17
kaido <kaido@qi-cloud.com>
2017-11-28 10:13:16 +0800

    update basic config template

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
+/**
+ * Created by Zep on 2017/11/28.
+ * 
+ */
+
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [{
-    path: '/',
-    name: 'HelloWorld',
-    component: HelloWorld
+    path: '/'
   }]
 })
---
+++ src/router/view.vue
@@ -0,0 +1,4 @@
+<!-- Created by Zep on 2017/11/28. -->
+<template>
+  <router-view class=""></router-view>
+</template>
